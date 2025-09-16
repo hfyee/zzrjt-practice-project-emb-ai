5f4b86d4b6824dbf3c95a93a6c446a15a4079e28
hfyee <hoongfai.yee@gmail.com>
2025-09-17 02:20:14 +0800

    Modified code based on pylint suggestions

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -1,19 +1,26 @@
-# Practice project
-# The functions of Watson AI libraries are deployed on IBM's Cloud IDE server, and cannot be imported here.
-
-import requests  # Import the requests library to handle HTTP requests
+"""
+Practice project
+The functions of Watson AI libraries are deployed on IBM's Cloud IDE server, 
+    and cannot be imported here.
+"""
 import json
+import requests  # Import the requests library to handle HTTP requests
 
-# Function for running sentiment analysis using the Watson NLP BERT Sentiment Analysis function
-def sentiment_analyzer(text_to_analyse):  # Define a function named sentiment_analyzer that takes a string input (text_to_analyse)
+def sentiment_analyzer(text_to_analyse):
+    """
+    Takes a string input (text_to_analyse)
+    """
     # URL of the sentiment analysis service
-    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'  
+    url = ( 
+        'https://sn-watson-sentiment-bert.labs.skills.network/v1/'
+        'watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    )
     # Create a dictionary with the text to be analyzed
-    myobj = { "raw_document": { "text": text_to_analyse } }  
+    myobj = { "raw_document": { "text": text_to_analyse } }
     # Set the headers required for the API request
-    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}  
+    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
     # Send a POST request to the API with the text and headers
-    response = requests.post(url, json = myobj, headers=header)  
+    response = requests.post(url, json = myobj, headers=header, timeout=5)
   
     # Parse the JSON response from the API
     formatted_response = json.loads(response.text)
